b3b56483b6305ff72356037d4e45a51d4e70924d
wangpeilei <o6996o@163.com>
2023-05-17 00:16:17 +0800

    add

---
+++ 47_permutations-ii.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// https://leetcode.cn/problems/permutations-ii/
+
+func permuteUnique(nums []int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+
+	tmp := make([]int, len(nums))
+	used := make([]bool, len(nums))
+
+	traceback47(&result, tmp, used, nums, 0)
+	return result
+}
+
+func traceback47(result *[][]int, tmp []int, used []bool, nums []int, idx int) {
+	if idx == len(tmp) {
+		t := make([]int, len(tmp))
+		for i, v := range tmp {
+			t[i] = v
+		}
+		*result = append(*result, t)
+		return
+	}
+
+	for i, num := range nums {
+		if used[i] || (i > 0 && nums[i] == nums[i-1] && !used[i-1]) { // 如果前一个数没被选过，那也不能选当前的数
+			continue
+		}
+
+		tmp[idx] = num
+		used[i] = true
+
+		traceback47(result, tmp, used, nums, idx+1)
+
+		used[i] = false
+	}
+}
+
+func main() {
+	unique := permuteUnique([]int{1, 1, 2})
+	fmt.Println(unique)
+}

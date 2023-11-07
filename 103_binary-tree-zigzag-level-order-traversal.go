06972405be82c23ed49deab5577045f25f5c367c
wangpeilei <o6996o@163.com>
2023-11-07 09:47:54 +0800

    add

---
+++ 103_binary-tree-zigzag-level-order-traversal.go
@@ -0,0 +1,50 @@
+package main
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	result := make([][]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	reversed := false
+	for len(queue) != 0 {
+		curLevelLen := len(queue) // 当前level节点数量
+		curLevelVals := make([]int, curLevelLen)
+		nextLevelList := make([]*TreeNode, 0) // 保存下一层level节点
+		for i, node := range queue {
+			curLevelVals[i] = node.Val
+			if node.Left != nil {
+				nextLevelList = append(nextLevelList, node.Left)
+			}
+			if node.Right != nil {
+				nextLevelList = append(nextLevelList, node.Right)
+			}
+		}
+
+		if reversed {
+			reverse103(curLevelVals) // 翻转当前level的vals
+		}
+		reversed = !reversed
+
+		result = append(result, curLevelVals)
+
+		if len(nextLevelList) > 0 {
+			queue = append(queue, nextLevelList...)
+		}
+		queue = queue[curLevelLen:]
+	}
+
+	return result
+}
+
+func reverse103(res []int) {
+	l, r := 0, len(res)-1
+	for l < r {
+		res[l], res[r] = res[r], res[l]
+		l++
+		r--
+	}
+}
---
+++ 104_maximum-depth-of-binary-tree.go
@@ -0,0 +1,16 @@
+package main
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return max104(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max104(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
